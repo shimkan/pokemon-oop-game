76056bcb9e0b7cd9176733f9f6cbc2268059d8e2
shimakan000 <kandai2003@gmail.com>
2025-05-15 19:03:49 -0700

    three pokemon choice from vector -> then verified

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <string>
-
-using namespace std;
+#include <vector>
+#include <algorithm>
+#include <cctype>
 
 int main() {
-  cout << "enter player name:" << endl;
-  string player_name;
-  string* str = new string("hello");
-  cin >> player_name;
-  cout << "welcome " << player_name << endl;
+
+  std::cout << "enter player name:" << std::endl;
+  std::string player_name, my_pokemon;
+  std::cin >> player_name;
+  std::cout << "welcome " << player_name << std::endl;
+  std::cout << "Choose your Pokemon!\nEnter your choice:\n" << std::endl;
+  std::vector<std::string> pokemon_list = {"charmander", "bulbasaur", "squirtle"};
+  for (const auto& pokemon_name : pokemon_list) {std::cout << pokemon_name << std::endl;}
+  std::cin >> my_pokemon;
+  std::transform(my_pokemon.begin(), my_pokemon.end(), my_pokemon.begin(),
+                  [](unsigned char c) {return std::tolower(c);});
+  if (std::find(pokemon_list.begin(), pokemon_list.end(), my_pokemon) == pokemon_list.end()) {
+    std::cerr << "invalid choice" << std::endl;
+  } else {
+    std::cout << "You have chosen " << my_pokemon << std::endl;
+  }
   return 0;
 }

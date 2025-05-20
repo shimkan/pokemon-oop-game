3e425e321c7f41dc77f1b5d7b955a4192b911f05
shimakan000 <kandai2003@gmail.com>
2025-05-19 21:39:10 -0700

    enum -> enum class + scope resolution

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 
-enum PokemonChoice {
+enum class PokemonChoice {
   Charmander,
   Bulbasaur,
   Squirtle,
@@ -18,25 +18,25 @@ int main() {
   std::vector<std::string> pokemon_list = {"1. charmander", "2. bulbasaur", "3. squirtle"};
   for (const auto& pokemon_name : pokemon_list) {std::cout << pokemon_name << std::endl;}
   int my_pokemon;
-  PokemonChoice chosen_pokemon = InvalidChoice;
+  PokemonChoice chosen_pokemon = PokemonChoice::InvalidChoice;
   std::cin >> my_pokemon;
   
   switch(my_pokemon) {
     case 1:
       std::cout << "you have chosen charmander! A fiery choice!" << std::endl;
-      chosen_pokemon = Charmander;
+      chosen_pokemon = PokemonChoice::Charmander;
       break;
     case 2:
       std::cout << "you have chosen bulbasaur! A clean choice!" << std::endl;
-      chosen_pokemon = Bulbasaur;
+      chosen_pokemon = PokemonChoice::Bulbasaur;
       break;
     case 3:
       std::cout << "you have chosen squirtle! A splashy choice!" << std::endl;
-      chosen_pokemon = Squirtle;
+      chosen_pokemon = PokemonChoice::Squirtle;
       break;
     default:
       std::cout << "you have been assigned Pikachu!" << std::endl;
-      chosen_pokemon = InvalidChoice;
+      chosen_pokemon = PokemonChoice::InvalidChoice;
       break;
   }
   return 0;
